a72aa8591d7344c1f1f563253d5f55dc52c7beac
kniazyouk <kniazyouk@gmail.com>
2026-08-30 20:49:01 -0400

    0.1.1

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
+  base: '/KniazYouk/',
   plugins: [react(), tailwindcss()],
-})
-
-
-// export default defineConfig({
-//   base: '/KniazYouk/',
-//   plugins: [react(), tailwindcss()],
-// })
+});
